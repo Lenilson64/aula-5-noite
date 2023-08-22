8cbbd36f751cf06d5a7612e4216744156efecb03
Lenilson64 <142455899+Lenilson64@users.noreply.github.com>
2023-08-21 21:44:48 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+/*
+Lenilson Almeida
+Este texto sera ignorado
+*/
+console.log("Olá Mundo!");
+//prompt("Qual é o seu nome?");
+
+//Variáveis
+const variavelA = 100;
+const variavelB = variavelA + 50;
+console.log(variavelB);
+let numero = 50;
+console.log(numero);
+numero = 80;
+console.log(numero);
+
+const nome = "Lenilson";
+const idade = 31;
+console.log("Olá!", "Meu nome é", nome, "e eu tenho", idade, "anos.");
+
+const primeiroNome = "Lenilson";
+const sobrenome = " de Almeida Brites";
+const minhaIdade = 31;
+const souestudante = true;
+console.log("\nNome =", nome + sobrenome,"\nIdade =", minhaIdade,"\nEstudante =", souestudante);
+
+/*
+let input1 = prompt("Digite algo:");
+console.log("Palavra:", input1,"\n", input1.toUpperCase, input1.toLowerCase, input1.length, input1.startsWith("a"), input1.indexOf("e"));
+*/
